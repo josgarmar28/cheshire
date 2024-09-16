3abce15cb28e856eb2528a6a95d1482f6a04e014
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2024-09-16 10:53:13 +0200

    Add testClass

---
+++ src/test/java/cheshire/MyTestClass.java
@@ -0,0 +1,13 @@
+package cheshire;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyTestClass {
+ @Test
+ public void testAddition() {
+ int result = 2 + 3;
+ result = 10;
+ assertEquals(10, result);
+ }
+}
